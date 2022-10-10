0bec964e1ed9e5607a208dba41d91d80cd0b3c6d
Nerijus Marcinkenas <nerijus.marcinkenas@gmail.com>
2022-10-10 19:58:51 +0300

    Init

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,25 @@
+document.querySelector("#login").addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Login submit');
+    const loginRequest = {
+        userName: event.target.name.value,
+        password: event.target.password.value
+    };
+
+    fetch("https://localhost:7004/api/Auth/Login", {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(loginRequest)
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.errorMessage) {
+                alert(result.errorMessage);
+                return;
+            }
+            sessionStorage.setItem('token', result.token);
+            window.location = "weather.html";
+        }).catch(error => console.log(error));
+});
---
+++ FrontEnd/js/weather.js
@@ -0,0 +1,25 @@
+const token = sessionStorage.getItem("token");
+
+if (!token) {
+    alert("Please login!");
+    window.location = "index.html";
+}
+else {
+    fetch("https://localhost:7004/WeatherForecast", {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    })
+        .then(respones => respones.json())
+        .then(result => {
+            const container = document.querySelector('#weather-container');
+            for (let i = 0; i < result.length; i++) {
+                const element = result[i];
+                const weatherParagraph = document.createElement('p');
+                weatherParagraph.innerHTML = `<b>${element.date}</b> temperature : <b>${element.temperatureC} C</b> weather will be <b> ${element.summary}</b>`;
+                container.append(weatherParagraph);
+            }
+        })
+        .catch(error => alert(error));
+}
